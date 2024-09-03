0fa2e6e6a188267c31561d1d12b71db2af373885
Kevin <unixcc@126.com>
2024-09-03 23:23:32 +0800

    Point test enhancement

--- circle/PointTest.cpp
+++ circle/PointTest.cpp
@@ -5,43 +5,43 @@
 TEST(PointTest, Constructor)
 {
     Point a;
-    ASSERT_EQ(a.getX(), 0);
-    ASSERT_EQ(a.getY(), 0);
+    ASSERT_EQ(a.getX(), 0.0);
+    ASSERT_EQ(a.getY(), 0.0);
 
     Point b(3, 4);
-    ASSERT_EQ(b.getX(), 3);
-    ASSERT_EQ(b.getY(), 4);
+    ASSERT_EQ(b.getX(), 3.0);
+    ASSERT_EQ(b.getY(), 4.0);
 }
 
 TEST(PointTest, SetterGetter)
 {
     Point a;
-    a.setX(3);
-    a.setY(4);
-    ASSERT_EQ(a.getX(), 3);
-    ASSERT_EQ(a.getY(), 4);
+    a.setX(3.0);
+    a.setY(4.0);
+    ASSERT_EQ(a.getX(), 3.0);
+    ASSERT_EQ(a.getY(), 4.0);
 }
 
 TEST(PointTest, DefaultOperator)
 {
     Point a;
-    a = {3, 4};
-    ASSERT_EQ(a.getX(), 3);
-    ASSERT_EQ(a.getY(), 4);
+    a = {3.0, 4.0};
+    ASSERT_EQ(a.getX(), 3.0);
+    ASSERT_EQ(a.getY(), 4.0);
 
-    Point b(10, 21);
+    Point b(10.0, 21.0);
     a = b;
-    ASSERT_EQ(a.getX(), 10);
-    ASSERT_EQ(a.getY(), 21);
+    ASSERT_EQ(a.getX(), 10.0);
+    ASSERT_EQ(a.getY(), 21.0);
 }
 
 TEST(PointTest, Distance)
 {
-    Point a(3, 4);
-    ASSERT_EQ(a.getDistance(), 5);
+    Point a(3.0, 4.0);
+    ASSERT_EQ(a.getDistance(), 5.0);
 
-    Point b(3, 8);
-    ASSERT_EQ(a.getDistance(b), 4);
+    Point b(3.0, 8.0);
+    ASSERT_EQ(a.getDistance(b), 4.0);
 }
 
 int main(int argc, char **argv)

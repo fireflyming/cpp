c61628803544abf11622a4086343538f60999c98
Kevin <unixcc@126.com>
2024-09-03 23:23:03 +0800

    Point enhancement

--- circle/Point.cpp
+++ circle/Point.cpp
@@ -2,13 +2,13 @@
 
 #include "Point.h"
 
-int Point::getDistance() const
+float Point::getDistance() const
 {
-    Point b(0, 0);
+    Point b(0.0, 0.0);
     return getDistance(b);
 }
 
-int Point::getDistance(const Point & b) const
+float Point::getDistance(const Point & b) const
 {
     return sqrt(pow((m_x - b.m_x), 2) + pow((m_y - b.m_y), 2));
 }
--- circle/Point.h
+++ circle/Point.h
@@ -4,27 +4,30 @@
 class Point {
 public:
     Point() : Point(0, 0) {}
-    Point(int x, int y) : m_x(x), m_y(y) {}
+    Point(float x, float y) : m_x(x), m_y(y) {}
     virtual ~Point() {}
 
-    int getX() const { return m_x; }
-    int getY() const { return m_y; }
-    void setX(int x) { m_x = x; }
-    void setY(int y) { m_y = y; }
+    float getX() const { return m_x; }
+    float getY() const { return m_y; }
+    void setX(float x) { m_x = x; }
+    void setY(float y) { m_y = y; }
 
     /**
      * return the distance to (0, 0)
      */
-    int getDistance() const;
+    float getDistance() const;
 
     /**
      * return the distance to another Point
      */
-    int getDistance(const Point &) const;
+    float getDistance(const Point &) const;
 
+    bool operator==(const Point & a) const {
+        return m_x == a.m_x && m_y == a.m_y;
+    }
 
 private:
-    int m_x, m_y;
+    float m_x, m_y;
 };
 
 #endif
